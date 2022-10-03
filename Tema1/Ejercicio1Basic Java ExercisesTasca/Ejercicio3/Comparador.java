b885f438570ea0a270cfb074367edc8c799b7049
Carlos <infocarlosexpuig@gmail.com>
2022-10-03 19:53:16 +0200

    Ejercicio casi terminado.

--- Tema1/Ejercicio1Basic Java ExercisesTasca/Ejercicio3/Comparador.java
+++ Tema1/Ejercicio1Basic Java ExercisesTasca/Ejercicio3/Comparador.java
@@ -1,2 +1,27 @@
 public class Comparador implements  Relaciones{
+    public int entero = 5;
+    public double doble = 5.5;
+
+    public Comparador(int entero, double doble) {
+        this.entero = entero;
+        this.doble = doble;
+    }
+
+    @Override
+    public boolean esMayor(Object b) {
+        if (this.entero > ) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean esMenor(Object b) {
+        return false;
+    }
+
+    @Override
+    public boolean esIgual(Object b) {
+        return false;
+    }
 }
--- Tema1/Ejercicio1Basic Java ExercisesTasca/Ejercicio3/Relaciones.java
+++ Tema1/Ejercicio1Basic Java ExercisesTasca/Ejercicio3/Relaciones.java
@@ -1,2 +1,8 @@
 public interface Relaciones {
+    // Devuelve verdadero si a es mayor que b
+    boolean esMayor(Object b) ;
+    // Devuelve verdadero si a es menor que b
+    boolean esMenor(Object b) ;
+    // Devuelve verdadero si a es igual que b
+    boolean esIgual(Object b) ;
 }
