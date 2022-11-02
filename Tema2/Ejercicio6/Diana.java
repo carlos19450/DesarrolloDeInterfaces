d6b025b8236adba586fd9259477387ff8a14460d
Carlos <infocarlosexpuig@gmail.com>
2022-11-02 22:35:24 +0100

    Diana

--- Tema2/Ejercicio6/Diana.java
+++ Tema2/Ejercicio6/Diana.java
@@ -1,20 +1,39 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class Diana extends JFrame {
+    JSlider slider = new JSlider(JSlider.CENTER, 0, 100, 25);
+    JButton boton = new JButton("Mostrar valor"), botonDialog = new JButton("Volver a empezar");
+    JDialog jd = new JDialog(this,"Resultado", true);
+    JLabel jlInfo = new JLabel();
     public Diana() {
-        JSlider slider = new JSlider(JSlider.CENTER, 0, 50, 25);
+
         slider.setMinorTickSpacing(2);
         slider.setMajorTickSpacing(10);
         slider.setPaintTicks(true);
         slider.setPaintLabels(true);
-
-        JPanel panel = new JPanel();
-        setPreferredSize(new Dimension(960, 80));
-        panel.add(slider);
+        boton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jd.setSize(300, 200);
+                jlInfo.setText("Su valor es: " + slider.getValue());
+                jd.add(jlInfo);
+                jd.add(botonDialog);
+                jd.setLocationRelativeTo(null);
+                jd.setVisible(true);
+            }
+        });
+        setPreferredSize(new Dimension(960, 150));
+        add(slider, BorderLayout.SOUTH);
+        add(boton);
+        boton.setBounds(350, 800, 150, 100);
         setLocationRelativeTo(null);
-        add(panel);
+
     }
     public static void main(String[] args) {
         Diana frame = new Diana();
