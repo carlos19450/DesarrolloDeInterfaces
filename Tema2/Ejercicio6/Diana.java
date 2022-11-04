2c7142ec0f2db030f0c6a8ef82c91f3fb6f377e9
Carlos <infocarlosexpuig@gmail.com>
2022-11-04 14:25:44 +0100

    Diana

--- Tema2/Ejercicio6/Diana.java
+++ Tema2/Ejercicio6/Diana.java
@@ -7,27 +7,58 @@ import java.awt.event.ActionListener;
 
 
 public class Diana extends JFrame {
-    private JSlider slider = new JSlider(JSlider.CENTER, 0, 100, 25);
-    private JButton boton = new JButton("Mostrar valor"), botonDialog = new JButton("Volver a empezar");
+    private JFrame frame1 = new JFrame("Información");
+    private JSlider slider = new JSlider(JSlider.CENTER, 0, 100, 0);
+    private JButton boton = new JButton("Mostrar valor"), botonDialog = new JButton("Volver a empezar"), botonInfo = new JButton("Acerca de");
     private JDialog jd = new JDialog(this,"Resultado", true);
-    private JLabel jlInfo = new JLabel();
-    private JCheckBox level1 = new JCheckBox("Nivel 1"), level2 = new JCheckBox("Nivel 3"), level3 = new JCheckBox("Nivel 3");
+    private JLabel jlInfo = new JLabel(), jlAcercaDe = new JLabel();
+    private JCheckBox level1 = new JCheckBox("Nivel 1"), level2 = new JCheckBox("Nivel 2"), level3 = new JCheckBox("Nivel 3");
     private ButtonGroup levelGroup = new ButtonGroup();
     public Diana() {
         getContentPane();
-        setLayout(new GridLayout(5, 1, 10, 10));
-        slider.setMinorTickSpacing(1);
-        slider.setMajorTickSpacing(5);
+        setLayout(null);
         slider.setPaintTicks(true);
         slider.setPaintLabels(true);
+        slider.setBounds(250, 0, 480, 150);
+        boton.setBounds(400, 150, 150, 100);
+        botonInfo.setBounds(850, 0, 100, 25);
+        level1.setBounds(350, 300, 100, 50);
+        level2.setBounds(450, 300, 100, 50);
+        level3.setBounds(550, 300, 100, 50);
         add(slider, BorderLayout.SOUTH);
         add(boton);
+        add(botonInfo);
         levelGroup.add(level1);
         add(level1);
         levelGroup.add(level2);
         add(level2);
         levelGroup.add(level3);
         add(level3);
+        level2.setSelected(true);
+        level1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                slider.setMinorTickSpacing(1);
+                slider.setMajorTickSpacing(25);
+                slider.setBounds(425, 0, 96, 150);
+            }
+        });
+        level2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                slider.setMinorTickSpacing(1);
+                slider.setMajorTickSpacing(25);
+                slider.setBounds(250, 0, 480, 150);
+            }
+        });
+        level3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                slider.setMinorTickSpacing(1);
+                slider.setMajorTickSpacing(25);
+                slider.setBounds(0, 0, 940, 150);
+            }
+        });
         boton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -51,8 +82,26 @@ public class Diana extends JFrame {
                 jd.setVisible(true);
             }
         });
-        boton.setBounds(350, 800, 150, 100);
-        setPreferredSize(new Dimension(960, 450));
+        botonInfo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String informacion = "<html>Carlos Expósito<br><br>" +
+                        "Elementos usados en esta practica: <br>" +
+                        "JFrame -> Para crear esta ventana. <br>" +
+                        "JSlider -> Para crear la barra. <br>" +
+                        "JButton -> Para crear la interaccion con la barra. <br>" +
+                        "JDialog -> Para crear una ventana, diciendo si has acertado o no el número. <br>" +
+                        "JLabel -> Para crear comentarios dentro del programa. <br>" +
+                        "JCheckBox -> Para implementar los tipos de nivel. <br>" +
+                        "ButtonGroup -> Para elegir solo un nivel.</html>";
+                jlAcercaDe.setText(informacion);
+                frame1.add(jlAcercaDe);
+                frame1.setSize(500, 400);
+                frame1.setLocationRelativeTo(null);
+                frame1.setVisible(true);
+            }
+        });
+        setPreferredSize(new Dimension(960, 400));
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
