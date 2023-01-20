980cd6a6478dd199d667ba39909e4eeb7785f3ea
Carlos <infocarlosexpuig@gmail.com>
2023-01-20 19:10:16 +0100

    Ejercicio 4 creado

---
+++ Tema5/Ejercicio3/Ejercicio3.js
@@ -0,0 +1,19 @@
+var f1 = document.f1;
+console.log(f1)
+var f2;
+var f3;
+var f4;
+var f5;
+var f6;
+var f7;
+var f8;
+var f9;
+var f10;
+var f11;
+var f12;
+var f13;
+var f14;
+var f15;
+var f16;
+var f17;
+
